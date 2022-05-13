6a95b794c8ddeb132a5cab2372d86e9aaf2e9767
BaselWaheed <baselwaheed66@gmail.com>
2022-05-13 15:29:15 +0200

    updateplace

--- view/admin.py
+++ view/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Commenteplace, Favouriteplace, Offers, Turism  ,Places , Rate , Event
+from .models import City, Commenteplace, Favouriteplace, Offers, Turism  ,Places , Rate , Event
 
 
 
@@ -16,7 +16,7 @@ class RateAdmin(admin.ModelAdmin):
 admin.site.register(Rate,RateAdmin)
 ############################################################################
 class PlacesAdmin(admin.ModelAdmin):
-    list_display = ['place_name' ,  'type' , 'is_active']
+    list_display = ['place_name' , 'city' ,'type' , 'is_active']
     list_filter = ['type']
     search_fields = ['place_name']
 
@@ -42,3 +42,7 @@ admin.site.register(Favouriteplace)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['id','user' ,'place']
 admin.site.register(Commenteplace,CommentAdmin)
+
+
+
+admin.site.register(City)
